6632361bc3a5974aaa14c1fdd757fa9072dd3796
TomasCiccarella <taciccarella@gmail.com>
2026-01-21 17:30:16 -0300

    Agrego lo de CosmoLattice

---
+++ cosmolattice/build_m2phi2/CMakeFiles/cosmolattice.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cosmolattice.dir/link.d"
+  "CMakeFiles/cosmolattice.dir/src/cosmolattice.cpp.o"
+  "CMakeFiles/cosmolattice.dir/src/cosmolattice.cpp.o.d"
+  "m2phi2"
+  "m2phi2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cosmolattice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
